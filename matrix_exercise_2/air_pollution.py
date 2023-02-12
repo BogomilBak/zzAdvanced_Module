8441ad7751b2f7bd92a21a100ff5928d357b4922
BogomilBak <levinebogi1@gmail.com>
2023-02-13 01:28:10 +0200

    Push

--- matrix_exercise_2/air_pollution.py
+++ matrix_exercise_2/air_pollution.py
@@ -12,7 +12,25 @@ for action in operations:
     value = int(value)
 
     if command == "breeze":
-        pass
+        for x in range(5):
+            matrix[value][x] -= 15
     elif command == "gale":
-        pass
-    elif command == "smog"
+        for x in range(5):
+            matrix[x][value] -= 20
+    elif command == "smog":
+        for x in range(5):
+            for y in range(5):
+                matrix[x][y] += value
+
+polluted = []
+
+for x in range(5):
+    for y in range(5):
+        if matrix[x][y] >= 50:
+            z = f"[{x}-{y}]"
+            polluted.append(z)
+
+if polluted:
+    print(', '.join(polluted))
+else:
+    print("No polluted areas")
