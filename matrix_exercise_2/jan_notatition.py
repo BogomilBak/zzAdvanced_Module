38c5f9da3845d83ce830529698614228f1edff1e
BogomilBak <levinebogi1@gmail.com>
2023-02-13 02:21:34 +0200

    Push

---
+++ matrix_exercise_2/jan_notatition.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+#input_line = [3, 4, '+']
+#input_line = [5, 3, 4, '*', '-']
+#input_line = [7, 33, 8, '-']
+#input_line = [15, '/']
+#input_line = [31, 2, '+', 11, '/']
+input_line = [-1, 1, '+', 101, '*', 18, '+', 3, '/']
+
+numbers = deque()
+
+operation= {
+    "+": lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
+    '/': lambda a, b: a / b
+}
+
+for element in input_line:
+    if element not in operation:
+        numbers.append(element)
+    else:
+        if len(numbers) < 2:
+            print(f"Error: not enough operands!")
+            sys.exit()
+        first = numbers.pop()
+        second = numbers.pop()
+        result = operation[element](second, first)
+        numbers.appendleft(result)
+
+if len(numbers) == 1:
+    print(*numbers)
+else:
+    print("Error: too many operands!")
+
